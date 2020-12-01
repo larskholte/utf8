85c3dbb37e372378e2be91be235e204de096dd7d
Lars Holte <larskholte@gmail.com>
2020-12-01 14:58:07 -0600

    Detect overlong sequences, improve error handling, improve comments

--- utf8.c
+++ utf8.c
@@ -4,6 +4,7 @@
 
 void UTF8_Decoder_init(UTF8_Decoder* decoder) {
 	decoder->state = 0;
+	decoder->seqLength = 0;
 	decoder->c = 0;
 }
 ucp* UTF8_Decoder_decode(UTF8_Decoder* decoder, byte b, ucp* c, int* error) {
@@ -17,32 +18,39 @@ ucp* UTF8_Decoder_decode(UTF8_Decoder* decoder, byte b, ucp* c, int* error) {
 		// First byte of multi-byte character
 		if ((b & 0xe0) == 0xc0) {
 			decoder->c = b & 0x1f;
+			decoder->seqLength = 2;
 			decoder->state = 1;
 			return c;
 		}
 		if ((b & 0xf0) == 0xe0) {
 			decoder->c = b & 0x0f;
+			decoder->seqLength = 3;
 			decoder->state = 2;
 			return c;
 		}
 		if ((b & 0xf8) == 0xf0) {
 			decoder->c = b & 0x07;
+			decoder->seqLength = 4;
 			decoder->state = 3;
 			return c;
 		}
-		*error = UTF8_DECODER_ERROR_INVALID_START_BYTE;
+		*error = UTF8_ERROR_INVALID_START_BYTE;
 		return c;
 	}
 	// Next byte of multi-byte character
 	if ((b & 0xc0) != 0x80) {
-		*error = UTF8_DECODER_ERROR_EXPECTED_CONTINUATION_BYTE;
+		*error = UTF8_ERROR_INVALID_CONTINUATION_BYTE;
 		return c;
 	}
 	decoder->c = decoder->c << 6 | (b & 0x3f);
 	decoder->state--;
 	if (decoder->state == 0) {
 		// Multi-byte character complete
-		*(c++) = decoder->c;
+		if (UTF8_BytesForChar(decoder->c) == decoder->seqLength) {
+			*(c++) = decoder->c;
+		} else {
+			*error = UTF8_ERROR_OVERLONG_SEQUENCE;
+		}
 	}
 	return c;
 }
@@ -51,7 +59,6 @@ int UTF8_Decoder_canTerminate(UTF8_Decoder* decoder) {
 }
 
 ucp* UTF8_DecodeArray(byte* ba, size_t bc, ucp* ca, size_t cc, int* error) {
-	*error = 0;
 	UTF8_Decoder decoder;
 	UTF8_Decoder_init(&decoder);
 	byte* bend = ba + bc;
@@ -64,15 +71,16 @@ ucp* UTF8_DecodeArray(byte* ba, size_t bc, ucp* ca, size_t cc, int* error) {
 	}
 	if (ba < bend && ca >= cend) {
 		// We ran out of room to store characters
-		*error = UTF8_DECODE_ERROR_CHAR_OVERFLOW;
+		*error = UTF8_ERROR_CHAR_OVERFLOW;
 	} else if (!UTF8_Decoder_canTerminate(&decoder)) {
 		// Input bytes did not end on a character boundary
-		*error = UTF8_DECODE_ERROR_UNEXPECTED_TERMINATION;
+		*error = UTF8_ERROR_UNEXPECTED_TERMINATION;
+	} else {
+		*error = 0;
 	}
 	return ca;
 }
 ucp* UTF8_DecodeString(const char* str, ucp* ca, size_t cc, int* error) {
-	*error = 0;
 	UTF8_Decoder decoder;
 	UTF8_Decoder_init(&decoder);
 	ucp* cend = ca + cc;
@@ -83,9 +91,11 @@ ucp* UTF8_DecodeString(const char* str, ucp* ca, size_t cc, int* error) {
 		}
 	}
 	if (*str) {
-		*error = UTF8_DECODE_ERROR_CHAR_OVERFLOW;
+		*error = UTF8_ERROR_CHAR_OVERFLOW;
 	} else if (!UTF8_Decoder_canTerminate(&decoder)) {
-		*error = UTF8_DECODE_ERROR_UNEXPECTED_TERMINATION;
+		*error = UTF8_ERROR_UNEXPECTED_TERMINATION;
+	} else {
+		*error = 0;
 	}
 	return ca;
 }
@@ -119,7 +129,6 @@ byte* UTF8_EncodeChar(ucp c, byte* b) {
 	return b;
 }
 byte* UTF8_EncodeArray(ucp* ca, size_t cc, byte* ba, size_t bc, int* error) {
-	*error = 0;
 	ucp* cend = ca + cc;
 	byte* bend = ba + bc;
 	for (; ca < cend && ba + UTF8_BytesForChar(*ca) <= bend; ca++) {
@@ -127,7 +136,9 @@ byte* UTF8_EncodeArray(ucp* ca, size_t cc, byte* ba, size_t bc, int* error) {
 	}
 	if (ca < cend) {
 		// We did not encode all characters
-		*error = UTF8_ENCODE_ERROR_BYTE_OVERFLOW;
+		*error = UTF8_ERROR_BYTE_OVERFLOW;
+	} else {
+		*error = 0;
 	}
 	return ba;
 }
